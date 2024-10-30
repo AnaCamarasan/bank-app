b54d4f7124c40d70d58e8c10140b4ef6c7722ff7
jtma <jtm.astley@outlook.com>
2024-10-30 14:21:33 +0000

    created savingsAccount

---
+++ src/main/java/savingsAccount.java
@@ -0,0 +1,20 @@
+
+import src.main.java.BankAccount;
+
+class savingsAccount extends BankAccount {
+
+    private double interestRate;
+
+
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+
+
+}
