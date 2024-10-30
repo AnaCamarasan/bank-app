b21b6b4e044afe118b29ca02bc13bb05d733eccf
Dylan Todd <dylantodd00@gmail.com>
2024-10-30 14:27:23 +0000

    Worked on currentAccount class

--- src/main/java/CurrentAccount.java
+++ src/main/java/CurrentAccount.java
@@ -1,5 +1,15 @@
 public class CurrentAccount extends BankAccount{
     private double maximumWithdrawalAmount;
 
+    // All args constructor
+    public CurrentAccount(double accountBalance, double minimumBalance, String accountHolderName, double maximumWithdrawalAmount) {
+        super(accountBalance, minimumBalance, accountHolderName);
+        this.maximumWithdrawalAmount = maximumWithdrawalAmount;
+    }
 
+    // Constructor with default max withdrawal amount
+    public CurrentAccount(double accountBalance, double minimumBalance, String accountHolderName) {
+        super(accountBalance, minimumBalance, accountHolderName);
+        this.maximumWithdrawalAmount = 200;
+    }
 }
