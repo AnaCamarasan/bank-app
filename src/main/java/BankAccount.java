6a443aedd3118cf338b2335ba60acedba917094e
AnaCamarasan <ana.camarasan@gmail.com>
2024-10-30 14:20:01 +0000

    created bank account class

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,44 @@
 public class BankAccount {
+    private double accountBalance;
+    private double minimumBalance;
+    private String accountHolderName;
+
+    public BankAccount(double accountBalance, double minimumBalance, String accountHolderName) {
+        this.accountBalance = accountBalance;
+        this.minimumBalance = minimumBalance;
+        this.accountHolderName = accountHolderName;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    public void setMinimumBalance(double minimumBalance) {
+        this.minimumBalance = minimumBalance;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountBalance=" + accountBalance +
+                ", minimumBalance=" + minimumBalance +
+                ", accountHolderName='" + accountHolderName + '\'' +
+                '}';
+    }
 }
